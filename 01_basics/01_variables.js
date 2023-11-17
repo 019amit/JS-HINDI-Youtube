3deb29833ad2942244f116686fee663b43b6e3bb
019amit <93581165+019amit@users.noreply.github.com>
2023-11-17 21:02:01 +0000

    container  config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let email="a@gmail.com"
+var accountpassword="12345"
+accountcity="mhow"
+//accountId=1234 const keyword cant be reassign and redeclared
+email="s@gmail.com"
+accountpassword="3456"
+accountcity="indore";
+
+console.table([accountId,email,accountpassword,accountcity])
+/* prefer not to use var because of issue of block scope and functional scope
+*/
