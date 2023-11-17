df810cccf0375111e7aaf4b4d2b6a7b9de3edf1f
019amit <93581165+019amit@users.noreply.github.com>
2023-11-17 21:55:55 +0000

    container config done

---
+++ 01_basics/03_convesions.js
@@ -0,0 +1,21 @@
+// let score="33abc"
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueinnumber=Number(score)
+// console.log(typeof(valueinnumber))
+// console.log(valueinnumber)
+
+// let score=null
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueinnumber=Number(score)
+// console.log(typeof(valueinnumber))
+// console.log(valueinnumber)
+
+let score=undefined
+console.log(typeof score)
+console.log(typeof(score))
+let valueinnumber=Number(score)
+console.log(typeof(valueinnumber))
+console.log(valueinnumber)
+// 
