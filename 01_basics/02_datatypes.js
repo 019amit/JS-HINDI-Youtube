d80dcaaaf91b012aa599b988c5a3e149e4d62bde
019amit <93581165+019amit@users.noreply.github.com>
2023-11-17 21:30:16 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"//treat all js code as newer version
+//alert(3+3)  we are using node js not browser
+let name="amit"
+let age=18
+let isloggedin=false
+//number=>2 to power 53
+//bigint use for large value
+//string=>""
+//boolean=>true/false
+//null=>standalone
+//undefined=>
+//symbol to identify unique components
+
+//object
+console.log(typeof null) //object
+console.log(typeof undefined)//undefined
